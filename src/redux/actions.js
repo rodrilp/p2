9708a6352751e9e361eae16f571c2a7ae1494ab9
Rodrigo <rodrigolp7@gmail.com>
2019-10-03 20:19:36 +0200

    nuevas actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,14 @@
 export const QUESTION_ANSWER = "QUESTION_ANSWER";
+export const CHANGE_QUESTION = "CHANGE_QUESTION";
+export const SUBMIT = "SUBMIT"; 
 
 export function questionAnswer(index, answer) {
   return { type: QUESTION_ANSWER, payload: { index, answer } };
+}
+export function changeQuestion( isNext){
+	return { type:CHANGE_QUESTION,  isNext: isNext };
+}
+
+export function submit (questions){
+	return { type: SUBMIT, questions: questions };
 }
