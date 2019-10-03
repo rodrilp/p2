8b892dc8dbcdd6018eb08b82f85abe088baff19f
Rodrigo <rodrigolp7@gmail.com>
2019-10-03 11:59:48 +0200

    App hecho función

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import { connect } from 'react-redux';
 import Game from './Game';
 import {questionAnswer} from './redux/actions'
 
-function App(props) {
-  console.log(props)
-  return (
-    <div className='App'>
-      <Game question = {props.questions[props.currentQuestion]}
+
+
+export class App extends Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className = 'App'>
+        <div className = 'Navbar'>
+          <h1>QUIZ GAME</h1>
+        </div>
+        <Game question = {this.props.questions[this.props.currentQuestion]}
             onQuestionAnswer={(answer) => {
-              props.dispatch(questionAnswer(props.currentQuestion, answer));
+              this.props.dispatch(questionAnswer(this.props.currentQuestion, answer));
             }}
       />
-    </div>
-    
-    
-  );
+      </div>
+    )
+  }
 }
+
 
 function mapStateToProps(state) {
   return {
   ...state
   };
 }
+
 export default connect(mapStateToProps)(App);
