a742af8467e25c2856dc30df157e51de43584b3b
Rodrigo <rodrigolp7@gmail.com>
2019-10-09 12:58:21 +0200

    practice view

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import {
-    Link
-  }
-  from "react-router-dom"
+import {Link} from "react-router-dom"
+import './Home.css'
 
 export default class Home extends Component {
     render() {
@@ -11,20 +9,22 @@ export default class Home extends Component {
                 <div className = 'Navbar'>
                     <h1>QUIZ GAME</h1>
                 </div>
-                <h2>Home</h2>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/play/">Play</Link>
-                        </li>
-                        <li>
-                            <Link to = "/practice">Practice</Link>
-                        </li>
-                    </ul>
-                </nav>
+                <div className ='index'>
+                    <h2>Home</h2>
+                    <nav>
+                        <ul>
+                            <li className = 'item'>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className = 'item'> 
+                                <Link to="/play/">Play</Link>
+                            </li>
+                            <li className = 'item'>
+                                <Link to = "/practice">Practice</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         )
     }
--- src/Practice.js
+++ src/Practice.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.css';
+import { connect } from 'react-redux';
+import Game from './Game';
+import Botonera from './Botonera'
+import Mark from './Mark'
+import {questionAnswer, changeQuestion, submit, initQuestion, reset} from './redux/actions'
 
-export default class Practice extends Component {
-    render() {
-        return (
-            <div className='practica'>
-                <h1>Aqui va el juego con los botones y sin tiempo</h1>
-            </div>
-        )
+export class Practice extends Component {
+    loadQuizzes(){
+      fetch('https://quiz.dit.upm.es/api/quizzes/random10wa?token=32403b83b30b3e467e6c')
+      .then((response) =>{
+        return response.json();
+      })
+      .then((data) => this.props.dispatch(initQuestion(data)))
     }
+    componentDidMount(){
+      this.loadQuizzes();
+    }  
+
+    render() {
+        //console.log(this.props);
+    console.log(this.props.questions)
+    if(!this.props.finished){
+      return (
+        <div className = 'App'>
+          <div className = 'Navbar'>
+            <h1>QUIZ GAME</h1>
+          </div>
+          <Game question = {this.props.questions[this.props.currentQuestion]}
+                currentQuestion = {this.props.currentQuestion}
+                onQuestionAnswer={(answer) => {
+                  this.props.dispatch(questionAnswer(this.props.currentQuestion, answer));
+                }}
+          />
+          <Botonera question = {this.props.questions[this.props.currentQuestion]}
+                    currentQuestion = {this.props.currentQuestion}
+                    length = {this.props.questions.length}
+                    finished = {this.props.finished}
+                    onChangequestion = {(next) =>this.props.dispatch(changeQuestion(next))}
+                    onSubmit = {() => this.props.dispatch(submit(this.props.questions))}
+                    onReset = {() => {
+                      this.loadQuizzes()
+                      this.props.dispatch(reset())}}
+          />
+        </div>
+      )
+    }else {
+      console.log(this.props.finished)
+      return (
+        <div className = 'App'>
+          <div className = 'Navbar'>
+            <h1>QUIZ GAME</h1>
+          </div>
+        <Mark score = {this.props.score}
+              onReset = {() => {
+                this.loadQuizzes()
+                this.props.dispatch(reset())}}/>
+        </div>
+        
+      )
+    }
+    
+    
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+  ...state
+  };
+}
+
+export default connect(mapStateToProps)(Practice);
